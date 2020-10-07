c21e6a0fff85700123fe4c340985dd8d1b684a26
Khemanorak Khath <khath.khemanorak@gmail.com>
2020-10-07 14:48:17 +0700

    Fix typo

--- main.ts
+++ main.ts
@@ -5,10 +5,10 @@ export function add(numbers: string): number {
     // Test if provided number has custom delimiter
     const hasCustomDelimiter = /^(\/\/)(.+)\n/;
     const delimiter = hasCustomDelimiter.test(numbers) ? numbers.split(hasCustomDelimiter)[2].split(/[\[\]]+/).join(',') : null;
-    // Split into array of number values
+    // Split string to array of values
     const splitRegex = new RegExp('[\n,' + delimiter + ']+');
     const values = numbers.split(splitRegex);
-    // Only get value larger than less or equal to 1000
+    // Only get value less or equal to 1000
     const validValues = values.map(value => Number(value)).filter(val => val <= 1000);
     // Check if there are value larger than 1000
     const invalidValues = validValues.filter(val => val < 0);
